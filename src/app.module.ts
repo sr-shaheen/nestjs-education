e4a5da624439223cd6669dcb46f6d4cfd5749c77
Saidur Rahman Shaheen <shaheenfgs@outlook.com>
2021-07-10 17:00:11 +0600

    db configured

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,22 @@ import { AppService } from './app.service';
 import { UniversityModule } from './university/university.module';
 import { CoursesModule } from './courses/courses.module';
 import { StudentModule } from './student/student.module';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
-  imports: [UniversityModule, CoursesModule, StudentModule],
+  imports: [
+    MongooseModule.forRoot(
+      'mongodb+srv://shaheen:shaheen123@practice.6n21r.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
+      {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+        useFindAndModify: false,
+      },
+    ),
+    UniversityModule,
+    CoursesModule,
+    StudentModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/university/university.module.ts
+++ src/university/university.module.ts
@@ -1,7 +1,9 @@
 import { Module } from '@nestjs/common';
 import { UniversityController } from './university.controller';
+import { UniversityService } from './university.service';
 
 @Module({
   controllers: [UniversityController],
+  providers: [UniversityService],
 })
 export class UniversityModule {}
